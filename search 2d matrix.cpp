acbad82336775d70dd05c2b9b972ac5e915e0a88
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-11-13 23:03:59 +0530

    Update search 2d matrix.cpp

--- search 2d matrix.cpp
+++ search 2d matrix.cpp
@@ -33,3 +33,25 @@ public:
         return false;
     }
 };
+
+//O(log(n*m))
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int low=0,row=matrix.size(),mid,col=matrix[0].size(),high=row*col-1;
+        if(matrix.size()==0)
+            return false;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(matrix[mid/col][mid%col]==target)
+                return true;
+            else if(matrix[mid/col][mid%col]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return false;
+    }
+};
