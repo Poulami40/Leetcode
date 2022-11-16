2deb42f68b4b4c215c2166c6e1fa426dcd5a7b9a
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-11-16 10:39:45 +0530

    Update valid paranthesis.cpp

--- valid paranthesis.cpp
+++ valid paranthesis.cpp
@@ -35,3 +35,27 @@ public:
             return false;
     }
 };
+
+
+class Solution {
+public:
+    
+    bool isValid(string s) {
+        stack<char>st;
+        if(s.length()==0)
+            return false;
+        for(int i=0;i<s.length();i++)
+        {
+            if(st.empty())
+                st.push(s[i]);
+            else if((st.top()=='('&&s[i]==')')||(st.top()=='{'&&s[i]=='}')||(st.top()=='['&&s[i]==']'))
+                st.pop();
+            else
+                st.push(s[i]);
+        }
+        if(st.empty())
+            return true;
+        else
+            return false;
+    }
+};
