e1c3c36e683e5ff747214637864873b3ef2fc2c1
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-11-16 09:36:11 +0530

    Create merge 2 lists.cpp

---
+++ merge 2 lists.cpp
@@ -0,0 +1,85 @@
+//Iterative
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ptr=list1,*curr;
+       if(list2==NULL)
+           return list1;
+        if(list1==NULL)
+            return list2;
+        if(list2->val<list1->val)
+        {
+            ptr=list2;
+            list2=list2->next;
+        }
+        else
+        {
+            list1=list1->next;
+        }
+        curr=ptr;
+        while(list1&&list2)
+        {
+            if(list1->val<list2->val)
+            {
+                curr->next=list1;
+                list1=list1->next;           
+            }
+            else{
+                curr->next=list2;
+                list2=list2->next;
+            }
+            curr=curr->next;
+        }
+        if(!list1)
+            curr->next=list2;
+        else
+            curr->next=list1;
+        
+        return ptr;
+    }
+};
+
+//Recursion
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ptr=list1,*curr;
+       if(list2==NULL)
+           return list1;
+        if(list1==NULL)
+            return list2;
+        if(list2->val<=list1->val)
+        {
+            list2->next=mergeTwoLists(list2->next,list1);
+            return list2;
+        }
+        else
+        {
+             list1->next=mergeTwoLists(list1->next,list2);
+            return list1;
+        }
+        
+    }
+};
