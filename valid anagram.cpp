eeb47f99082bf78440bfa4e4355307770cd45fe2
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-11-16 08:57:52 +0530

    Create valid anagram.cpp

---
+++ valid anagram.cpp
@@ -0,0 +1,39 @@
+//O(m*n)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>m;
+        if(s.length()!=t.length())
+            return false;
+        for(int i=0;i<s.length();i++)
+        {
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            if(m[t[i]]==0)
+                return false;
+            else
+                m[t[i]]--;
+            if(m[t[i]]<0)
+                return false;
+        }
+        return true;
+            
+    }
+};
+
+//O(nlogn)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t)
+            return true;
+        return false;
+            
+    }
+};
