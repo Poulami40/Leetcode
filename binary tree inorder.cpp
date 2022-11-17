2a7de732438d6ecb970aaf70b5401abb91909f80
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-11-17 11:40:10 +0530

    Update and rename tree inorder.cpp to binary tree inorder.cpp

---
+++ binary tree inorder.cpp
@@ -0,0 +1,68 @@
+//Recursion
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>in;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root!=NULL)
+        {
+            inorderTraversal(root->left);
+            in.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return in;
+    }
+};
+
+//Iteration
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
+        TreeNode* node=root;
+        vector<int>in;
+        while(true)
+        {
+            if(node!=NULL)
+            {
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                if(st.empty())
+                    break;
+                node=st.top();
+                st.pop();
+                in.push_back(node->val);
+                node=node->right;
+            }
+           
+        }
+        return in;
+        
+    }
+};
