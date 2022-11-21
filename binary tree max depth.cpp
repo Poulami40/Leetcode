27904e7a95241a9b0c26719ec3dcb53221853ae8
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-11-21 11:53:19 +0530

    Update binary tree max depth.cpp

--- binary tree max depth.cpp
+++ binary tree max depth.cpp
@@ -22,3 +22,40 @@ public:
     }
 };
 
+//Iterative
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        int depth=0;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                if(q.front()->left!=NULL)
+                    q.push(q.front()->left);
+                if(q.front()->right!=NULL)
+                    q.push(q.front()->right);
+                q.pop();
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
